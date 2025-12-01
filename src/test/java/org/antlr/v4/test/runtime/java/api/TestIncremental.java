96fa810fe15a4f8713b1c77286b7ba7c63f865b2
Maximov Valery <maximovvalery@gmail.com>
2025-12-01 17:21:30 +0300

    license format

--- src/test/java/org/antlr/v4/test/runtime/java/api/TestIncremental.java
+++ src/test/java/org/antlr/v4/test/runtime/java/api/TestIncremental.java
@@ -1,4 +1,4 @@
-/**
+/*
  * This file is a part of ANTLR.
  *
  * Copyright (c) 2012-2025 The ANTLR Project. All rights reserved.
@@ -7,7 +7,6 @@
  * Use of this file is governed by the BSD-3-Clause license that
  * can be found in the LICENSE.txt file in the project root.
  */
-
 package org.antlr.v4.test.runtime.java.api;
 
 import org.antlr.v4.runtime.CharStreams;
